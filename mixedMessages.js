6adaabd77f9012a7294c1f5ca8cb5f39b8f7b8ec
Ryan Turner <turnerr8@wwu.edu>
2021-01-20 23:10:26 -0800

    finished phraseArray()

--- mixedMessages.js
+++ mixedMessages.js
@@ -10,7 +10,55 @@ function phraseArray() {
     let choiceOne = randomizer();
     let choiceTwo = randomizer();
     let choiceThree = randomizer();
+    const phrases = [];
     
+    switch(choiceOne) {
+        case 0:
+            phrases.push('You will ');
+            break;
+        case 1:
+            phrases.push('You shall ');
+            break;
+        case 2:
+            phrases.push('A close friend will ');
+            break;
+        case 3:
+            phrases.push('Someone new will ');
+            break;
+    }
+
+    switch(choiceTwo) {
+        case 0:
+            phrases.push('be rich ');
+            break;
+        case 1:
+            phrases.push('have good fortune ');
+            break;
+        case 2:
+            phrases.push('hurt someone near you ');
+            break;
+        case 3:
+            phrases.push('find a new friend ');
+            break;
+    }
+
+    switch(choiceThree) {
+        case 0:
+            phrases.push('in the near future.');
+            break;
+        case 1:
+            phrases.push('this winter.');
+            break;
+        case 2:
+            phrases.push('tomorrow.');
+            break;
+        case 3:
+            phrases.push('no time soon.');
+            break;
+    }
+
+
+    return phrases;
 }
 
-console.log(randomizer());
+console.log(phraseArray());
